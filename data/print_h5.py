6e5ed34f9b8f684360f312c5010374a22a022db7
Greyyy-HJC <hejinchen17@gmail.com>
2025-05-20 13:15:58 -0400

    add print h5

--- data/print_h5.py
+++ data/print_h5.py
@@ -1,20 +1,35 @@
-from typing import Any, Callable
+#!python
+# %%
 import h5py
-
-
-def print_h5(group: h5py.Group, level=0, logger: Callable[[Any], Any] = print):
-    """print h5 group to consol.
-
-    Args:
-        group (h5py.Group): _description_
-        level (int, optional): _description_. Defaults to 0.
-        logger (Callable[[Any], Any]): print like functions
-    """
-    if not isinstance(group, h5py.Group):
-        logger(level * "\t", group)
+import pandas as pd
+import sys
+# %%
+# %%
+def print_h5(group,level=0):
+    if(isinstance(group,h5py.Group)==False):
+        print(level*'\t',group)
         return
     else:
         for key in group.keys():
-            logger(level * "\t" + key + ":")
-            subgroup = group[key]
-            print_h5(subgroup, level + 1)
+            print(level*'\t'+key+':')
+            subgroup=group[key]
+            print_h5(subgroup,level+1)
+
+# %%
+if __name__=='__main__':
+    if len(sys.argv)==1 or \
+        (len(sys.argv)==2 and (sys.argv[1]=='-h' or sys.argv[1]=='--help')):
+        print("""usage: \
+to print the structure of HDF5 files.
+\tshell>> python print_h5.py file1 [file2 ...]""")
+        exit(0)
+
+    for file in sys.argv[1:]:
+        try:
+            print(file+':')
+            print_h5(h5py.File(file,'r'),1)
+            pass
+        except Exception:
+            print('\tERROR')
+            pass
+        
